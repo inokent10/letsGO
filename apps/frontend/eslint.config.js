c511611c3e825b9fe3a7aaad8f099ea0df179e7b
Karen <karen@MacBook-Pro-Karen.local>
2025-06-13 18:10:34 +0300

    init: создаст проект

---
+++ apps/frontend/eslint.config.js
@@ -0,0 +1,82 @@
+// import js from '@eslint/js'
+// import globals from 'globals'
+// import reactHooks from 'eslint-plugin-react-hooks'
+// import reactRefresh from 'eslint-plugin-react-refresh'
+// import tseslint from 'typescript-eslint'
+
+// export default tseslint.config(
+//   { ignores: ['dist'] },
+//   {
+//     extends: [
+//       js.configs.recommended,
+//       ...tseslint.configs.recommended,
+//       ...tseslint.configs.stylistic
+//     ],
+//     files: ['**/*.{ts,tsx}'],
+//     languageOptions: {
+//       ecmaVersion: 2020,
+//       globals: globals.browser,
+//     },
+//     plugins: {
+//       'react-hooks': reactHooks,
+//       'react-refresh': reactRefresh,
+//     },
+//     rules: {
+//       ...reactHooks.configs.recommended.rules,
+//       'react-refresh/only-export-components': [
+//         'warn',
+//         { allowConstantExport: true },
+//       ],
+//       'no-console': ['error', { allow: ['warn', 'error'] }],
+//       'semi': 'error',
+//       'quotes': ['error', 'single'], 
+//       'jsx-quotes': ['error', 'prefer-single']
+//     },
+//   },
+// )
+
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic // Добавляем стилистические правила
+    ],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: { jsx: true }
+      }
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'no-console': ['error', { allow: ['warn', 'error'] }],
+      
+      'semi': ['error', 'always'], 
+      'quotes': ['error', 'single'], 
+      'jsx-quotes': ['error', 'prefer-single'],
+      'indent': ['error', 2],
+      'object-curly-spacing': ['error', 'always'],
+      'array-bracket-spacing': ['error', 'never'],
+      'comma-dangle': ['error', 'always-multiline'],
+      'keyword-spacing': ['error', { before: true }],
+    },
+  },
+)
---
+++ apps/frontend/src/components/app/app.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>HELLO LET`S Go Project</h1>
+    </>
+  );
+}
+
+export default App;
