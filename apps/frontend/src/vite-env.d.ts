c27c9c39a2932d38b1c4db423447a72dd3392988
SJ23y <mcfolland@gmail.com>
2025-06-20 12:54:48 +0300

    fix: удаляет папку apps из ветки бэкенд

--- apps/frontend/src/vite-env.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="vite/client" />
-declare module '*.module.scss' {
-    const classes: Readonly<Record<string, string>>;
-    export default classes;
-  }
-  
-  declare module '*.scss';
