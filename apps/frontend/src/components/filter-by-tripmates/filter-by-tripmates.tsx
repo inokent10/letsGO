32d67227b90de4b16d51eb22eec350e037cb18d1
SJ23y <mcfolland@gmail.com>
2025-07-01 16:23:35 +0300

    feat: добавляет кастомный слайдер для левела

--- apps/frontend/src/components/filter-by-tripmates/filter-by-tripmates.tsx
+++ apps/frontend/src/components/filter-by-tripmates/filter-by-tripmates.tsx
@@ -2,14 +2,19 @@
 
 import { ApiRoute, ApiSettings, Hobby, Meal, MusicStyle, Vehicle } from '@/src/const';
 import './filter-by-tripmates.scss';
-import type { JSX } from 'react';
+import { useState, type JSX } from 'react';
 import FilterDropdown from '../filter-dropdown/filter-dropdown';
 import FilterInputsList from '../checkbox-list/filter-inputs-list';
+import CustomSlider from '../custom-slider/custom-slider';
 
 function FilterByTripmates(): JSX.Element {
-
+  const [level, setLevel] = useState<[number, number]>([30, 80]);
   const formSubmitHandler = (evt: React.FormEvent) => {
     evt.preventDefault();
+  };
+
+  const levelSliderChangeHandler = (_evt: Event, newValue: number | number[]) => {
+    setLevel(newValue as [number, number]);
   };
 
   return (
@@ -36,7 +41,21 @@ function FilterByTripmates(): JSX.Element {
         </FilterDropdown>
         <FilterDropdown title='Левел'>
           <div>
-           
+            <CustomSlider 
+              min={0}
+              max={100}
+              value={level}
+              ariaLabel=''
+              valueLabelDisplay='off'
+              onChange={levelSliderChangeHandler}
+              barWidth='192px'
+              barHeight='3px'
+              mainColor='#1d2e5b'
+              railColor='rgba(color: #1D2E5B, alpha: 0.2)'
+              tickSize='13px'
+            />
+
+            
           </div>
         </FilterDropdown>
         <button className='tripmates-filter-submit-btn' type='submit'>Показать</button>
